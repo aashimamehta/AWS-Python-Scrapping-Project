467449d1453fca9312da276f5afb94c0a0512f2d
Aashima Mehta <58320925+aashimamehta@users.noreply.github.com>
2020-09-04 13:11:13 -0700

    Update scrapeWebText.py

--- scrapeWebText.py
+++ scrapeWebText.py
@@ -1,11 +1,16 @@
+'''
+Author: Aashima Mehta
+Data of Last Moification : 9/4/2020
+'''
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import boto3
 
-page = requests.get('https://forecast.weather.gov/MapClick.php?lat=47.60357000000005&lon=-122.32944999999995#.X1GtyXlKhPY')
+page = requests.get('https://forecast.weather.gov/MapClick.php?lat=47.60357000000005&lon=-122.32944999999995#.X1GtyXlKhPY') #website link that I parsed in this project
 soup = BeautifulSoup(page.content, 'html.parser')
-#print(soup) to print the html code of that website //a good check
+#print(soup) to print the html code of that website                 //a good check
 week = soup.find(id ='seven-day-forecast-body')
 #print(week)
 
@@ -15,7 +20,7 @@ items = (week.find_all(class_='tombstone-container'))
 #now stored in items list
 #print(items[0])
 
-#now extract the info required
+#now extract the info required                                  #good checks
 #print(items[0].find(class_='period-name').get_text())
 #print(items[0].find(class_='short-desc').get_text())
 #print(items[0].find(class_='temp').get_text())
@@ -36,14 +41,13 @@ def printReport(period_names, short_desc, temperature):
 
 print(printReport(period_names, short_desc, temperature))
 
-'''
+
 # to send the message using AWS SNS Serives to the topic 
 def main(message):
-    topicArn = 'arn:aws:sns:us-west-2:467640369669:first-test-Topic'
+    topicArn = 'arn:aws:sns:XX-XXX-X:XXXXXXXXXXXX:XXXXXXXX'             #unique link for the topic arn goes here
     snsClient = boto3.client('sns','us-west-2')
     response = snsClient.publish(TopicArn=topicArn, Message=message,
                    Subject='Publish to group test' )
 
 
 main(message)
-'''
